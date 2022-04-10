aa7885d4a7297c34d8eafa5670197e7405e28607
Alex Liu <alexliusch@gmail.com>
2022-04-10 17:26:33 -0400

    refine code

--- src/main/java/org/checkerframework/checker/linear/LinearTransfer.java
+++ src/main/java/org/checkerframework/checker/linear/LinearTransfer.java
@@ -1,10 +1,27 @@
 package org.checkerframework.checker.linear;
 
+import java.util.Set;
+import javax.lang.model.element.AnnotationMirror;
+import org.checkerframework.checker.linear.qual.Unique;
+import org.checkerframework.dataflow.analysis.TransferInput;
+import org.checkerframework.dataflow.cfg.node.Node;
 import org.checkerframework.framework.flow.CFAnalysis;
+import org.checkerframework.framework.flow.CFStore;
 import org.checkerframework.framework.flow.CFTransfer;
+import org.checkerframework.framework.flow.CFValue;
 
 public class LinearTransfer extends CFTransfer {
+
+    /** The @{@link Unique} annotation. */
     public LinearTransfer(CFAnalysis analysis) {
         super(analysis);
+    }
+
+    @Override
+    public void processCommonAssignment(
+            TransferInput in, Node lhs, Node rhs, CFStore store, CFValue rhsValue) {
+        // update information in the store
+        Set<AnnotationMirror> annos = rhsValue.getAnnotations();
+        super.processCommonAssignment(in, lhs, rhs, store, rhsValue);
     }
 }
--- tests/general/SubtypingTest.java
+++ tests/general/SubtypingTest.java
@@ -13,7 +13,7 @@ class SubtypingTest {
         // then: rhs is unique, lhs b is nonlinear
         // 2. next, z = y, lhs @Nonlinear z, rhs y is @unique
         // then we let b = (y = z), i think we use a lub here, because rhs is unique
-        b = (z = y);
+        b = y;
 
         //        @Unique({"algo1", "algo2"})
         //        String e;
