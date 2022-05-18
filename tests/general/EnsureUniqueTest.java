d42a48bae495549e0dd297ec194574c1b9aaed45
Alex Liu <a299liu@uwaterloo.ca>
2022-05-18 15:25:26 -0400

    refine code

--- tests/general/EnsureUniqueTest.java
+++ tests/general/EnsureUniqueTest.java
@@ -1,34 +1,35 @@
-package general;
-
-import java.security.SecureRandom;
-import javax.crypto.spec.IvParameterSpec;
-import org.checkerframework.checker.linear.qual.Unique;
-
-class EnsureUniqueTest {
-    // For example, suppose state0 is a initial state and will be changed
-    // to state1 after being called by test1, while test 2 only accepts
-    // state0.
-    // One question is should it be only used on method parameter?
-    // multi args mean all allowed
-    // change the postcondition into @top and try to show some msg.
-    // relation between top and mayalias
-    /*
-    states = {"", "initialized", "used"}
-    * */
-    public void test1(byte @Unique [] bytes) {
-        byte @Unique({}) [] bytesIV = bytes;
-        SecureRandom secureRandom = new SecureRandom();
-        // After this method call,  byte @Unique [] bytesIV becomes  byte @Unique("initialized") []
-        secureRandom.nextBytes(bytesIV);
-        byte @Unique({}) [] newBytesIv;
-        // transfer state and the rhs becomes disappear
-        newBytesIv = bytesIV;
-        // ::error: unique.assignment.not.allowed
-        newBytesIv = bytesIV;
-        // newBytesIv becomes @Unique({"used"}) ,not finished yet!
-        // TODO: try debug options, write a local simulating class first
-        IvParameterSpec ivSpec = new IvParameterSpec(newBytesIv);
-        byte @Unique({}) [] testBytesIv;
-        testBytesIv = newBytesIv;
-    }
-}
+// package general;
+//
+// import java.security.SecureRandom;
+// import javax.crypto.spec.IvParameterSpec;
+// import org.checkerframework.checker.linear.qual.Unique;
+//
+// class EnsureUniqueTest {
+//    // For example, suppose state0 is a initial state and will be changed
+//    // to state1 after being called by test1, while test 2 only accepts
+//    // state0.
+//    // One question is should it be only used on method parameter?
+//    // multi args mean all allowed
+//    // change the postcondition into @top and try to show some msg.
+//    // relation between top and mayalias
+//    /*
+//    states = {"", "initialized", "used"}
+//    * */
+//    public void test1(byte @Unique [] bytes) {
+//        byte @Unique({}) [] bytesIV = bytes;
+//        SecureRandom secureRandom = new SecureRandom();
+//        // After this method call,  byte @Unique [] bytesIV becomes  byte @Unique("initialized")
+// []
+//        secureRandom.nextBytes(bytesIV);
+//        byte @Unique({}) [] newBytesIv;
+//        // transfer state and the rhs becomes disappear
+//        newBytesIv = bytesIV;
+//        // ::error: unique.assignment.not.allowed
+//        newBytesIv = bytesIV;
+//        // newBytesIv becomes @Unique({"used"}) ,not finished yet!
+//        // TODO: try debug options, write a local simulating class first
+//        IvParameterSpec ivSpec = new IvParameterSpec(newBytesIv);
+//        byte @Unique({}) [] testBytesIv;
+//        testBytesIv = newBytesIv;
+//    }
+// }
--- tests/general/SubtypingTest.java
+++ tests/general/SubtypingTest.java
@@ -1,70 +1,63 @@
-// package general;
-//
-// import org.checkerframework.checker.linear.qual.EnsureUnique;
-// import org.checkerframework.checker.linear.qual.MayAliased;
-// import org.checkerframework.checker.linear.qual.Unique;
-//
-// class SubtypingTest {
-//
-//    // full states are [initialized, state2, state3, state4], after get all of this
-//    // it can do nothing with the security random.
-//    // For example:
-//    // default state is {}, {initialized} means cannot be intizalied again,
-//    // similarly, state2 means cannot be state2 agaiin
-//    void test(@Unique({}) String x, @Unique({}) String y, @MayAliased String z) {
-//        @Unique({})
-//        String b;
-//        b = y;
-//        // ::error: unique.assignment.not.allowed
-//        b = y;
-//        // ::error: unique.parameter.not.allowed
-//        testInvocation(y);
-//        // ::error: unique.assignment.not.allowed
-//        b = y;
-//
-//        @Unique({})
-//        String bytesIV;
-//        bytesIV = x;
-//
-//        // For example, all states are {"initialized", "state2", "end"}, make explict state
-//        //        machine.
-//        // Supertype is unique({})
-//        //  @Unique({}) becomes  @Unique({"initialized"}), it can go to state2 or end, not
-//        // initialized again
-//        nextBytesSimulator(bytesIV);
-//        //  @Unique({"initialized"}) becomes  @Unique({"initialized", "state2"}), it can only end
-//        state2(bytesIV);
-//        // @Unique({"initialized", "state2"}) becomes  @Unique({"initialized", "state2", "end"})
-//        end(bytesIV);
-//    }
-//
-//    void testInvocation(String x2) {
-//        String b2;
-//        b2 = x2;
-//        return;
-//    }
-//
-//    @SuppressWarnings("contracts.postcondition.not.satisfied")
-//    @EnsureUnique(
-//            value = "#1",
-//            states = {"initialized"})
-//    public void nextBytesSimulator(@Unique({}) String str) {
-//        return;
-//    }
-//
-//    @SuppressWarnings("contracts.postcondition.not.satisfied")
-//    @EnsureUnique(
-//            value = "#1",
-//            states = {"initialized", "state2"})
-//    public void state2(@Unique({}) String str) {
-//        return;
-//    }
-//
-//    @SuppressWarnings("contracts.postcondition.not.satisfied")
-//    @EnsureUnique(
-//            value = "#1",
-//            states = {"initialized", "state2", "end"})
-//    public void end(@Unique({}) String str) {
-//        return;
-//    }
-// }
+package general;
+
+import org.checkerframework.checker.linear.qual.EnsureUnique;
+import org.checkerframework.checker.linear.qual.MayAliased;
+import org.checkerframework.checker.linear.qual.Unique;
+
+class SubtypingTest {
+
+    // full states are [initialized, state2, state3, state4], after get all of this
+    // it can do nothing with the security random.
+    // For example:
+    // default state is {}, {initialized} means cannot be intizalied again,
+    // similarly, state2 means cannot be state2 agaiin
+    void test(@Unique({}) String x, @Unique({}) String y, @MayAliased String z) {
+        @Unique({})
+        String b;
+        b = y;
+        // ::error: unique.assignment.not.allowed
+        b = y;
+        // ::error: unique.parameter.not.allowed
+        testInvocation(y);
+        // ::error: unique.assignment.not.allowed
+        b = y;
+
+        @Unique({})
+        String bytesIV;
+        bytesIV = x;
+
+        // For example, all states are {"initialized", "state2", "end"}, make explict state
+        //        machine.
+        // Supertype is unique({})
+        //  @Unique({}) becomes  @Unique({"initialized"}), it can go to state2 or end, not
+        // initialized again
+        //        nextBytesSimulator(bytesIV);
+        //        bytesIV = x;
+        IvParameterSpecSimulator ivParam = new IvParameterSpecSimulator(bytesIV);
+
+        bytesIV = x;
+    }
+
+    void testInvocation(String x2) {
+        String b2;
+        b2 = x2;
+        return;
+    }
+
+    @SuppressWarnings("contracts.postcondition.not.satisfied")
+    @EnsureUnique(
+            value = "#1",
+            states = {"initialized"})
+    public void nextBytesSimulator(@Unique({}) String str) {
+        return;
+    }
+
+    public class IvParameterSpecSimulator {
+
+        @SuppressWarnings("contracts.postcondition.not.satisfied")
+        @EnsureUnique(
+                value = "#1",
+                states = {"used"})
+        public IvParameterSpecSimulator(String iv) {}
+    }
+}
