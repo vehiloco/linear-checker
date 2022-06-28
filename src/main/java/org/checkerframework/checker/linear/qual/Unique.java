e15147bd61c48d33ba29f8d0290ca72584dd2dec
Alex Liu <a299liu@uwaterloo.ca>
2022-06-28 16:01:28 -0400

    refine code

--- src/main/java/org/checkerframework/checker/linear/qual/Unique.java
+++ src/main/java/org/checkerframework/checker/linear/qual/Unique.java
@@ -1,12 +1,14 @@
 package org.checkerframework.checker.linear.qual;
 
 import java.lang.annotation.*;
+import org.checkerframework.framework.qual.DefaultFor;
 import org.checkerframework.framework.qual.SubtypeOf;
 
 @Documented
 @SubtypeOf({MayAliased.class})
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+@DefaultFor(types = {String.class, byte[].class})
 public @interface Unique {
     String[] value() default {};
 }
