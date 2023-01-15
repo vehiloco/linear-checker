8b73fcbc9b67056b64d627da1f96a84e703d9ab2
Alex Liu <a299liu@uwaterloo.ca>
2023-01-15 11:19:40 -0500

    add a new conditional test case

--- tests/general/NullTest.java
+++ tests/general/NullTest.java
@@ -10,5 +10,7 @@ public class NullTest {
         //        @Unique Object o3 = new Object();
         //        Object o4;
         //        o4 = o3;
+        int a = 1;
+        Integer b = a > 1 ? 0 : null;
     }
 }
