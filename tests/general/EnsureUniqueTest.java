6bb9b4ca897b04fc70b4808eda211b55f08786a1
Billy Bai <y5bai@uwaterloo.ca>
2025-07-08 19:17:40 -0400

    Change the override from `visitmethod` to `processMethodTree`. An new error appears in `EnsureUniqueTest.java` but haven't looked into the logic behind

--- tests/general/EnsureUniqueTest.java
+++ tests/general/EnsureUniqueTest.java
@@ -27,6 +27,8 @@ class EnsureUniqueTest {
         // transfer state and the rhs becomes disappear
         newBytesIv = bytesIV;
         // newBytesIv becomes @Unique({"used"}).
+        // TODO: look into why there is an new error here
+        // :: error: (argument.type.incompatible)
         IvParameterSpec ivSpec = new IvParameterSpec(newBytesIv);
         byte @Unique({}) [] testBytesIv;
         testBytesIv = newBytesIv;
